d0e7aecead7265be3898bfe12feed0a7c2fd12d5
Maran.I.Msabeh <maramismail2015@gmail.com>
2024-11-24 15:26:58 +0200

    some button

--- app/components/Home/serviceCard.js
+++ app/components/Home/serviceCard.js
@@ -4,6 +4,7 @@ import React from "react";
 import Card from "react-bootstrap/Card";
 import { IoIosArrowForward } from "react-icons/io";
 import "../../styles/home.css";
+import Link from "next/link";
 
 function ServiceCard({ item }) {
   console.log(item, "item");
@@ -16,7 +17,8 @@ function ServiceCard({ item }) {
         <h5 className="card-title">{item.title}</h5>
       </Card.Header>
       <Card.Body>
-        <div className="service-title">
+     <div className="d-flex flex-col gap-[30px]">
+     <div className="service-title">
           Fake caption title
           <IoIosArrowForward />
         </div>
@@ -40,6 +42,15 @@ function ServiceCard({ item }) {
           Fake caption title
           <IoIosArrowForward />
         </div>
+     </div>
+
+     <div>
+      <Link href='/' className="card-link">
+       See all 11
+       <IoIosArrowForward className="text-['#405189']"/>
+
+      </Link>
+     </div>
       </Card.Body>
     </Card>
   );
